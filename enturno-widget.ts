24fcff95660644a731072ee00772028bc14f5f8f
Michal Kloc <mkloc.developer@gmail.com>
2019-11-09 10:31:22 +0100

    Widget code updated

--- enturno-widget.ts
+++ enturno-widget.ts
@@ -2,19 +2,6 @@
 
 <style>
 @media (min-width: 300px) {
-    .col-md-border:not(:last-child) {
-			border-right: 1px solid #d7d7d7;
-    }
-    .col-md-border + .col-md-border {
-			border-left: 1px solid #d7d7d7;
-			margin-left: -1px;
-    }
-  	.entur-to-upper:first-letter {
-    	text-transform: uppercase;
-		}
-  	.entur-condition {
-  		height: 2em;
- 		}
     .vertical-top {
       margin: 0;
       position: absolute;
@@ -26,64 +13,24 @@
 }
 </style>
 
-<div id="container">
-  <div class="vertical-top">
-    <div id="direction01">
+<div id="container" oc-lazy-load="['/static/enturno-widget/enturno.js']">
+  <div class="vertical-top" ng-controller="enturCtrl">
+    <div id="direction01" class="entur-top-padding">
 			<div class="row">
         <div class="col-md-1">
         </div>
         <div class="col-md-11">
-          <h2 class="text-left">{{itemValue('StopPlaceName')}}</h2>
-          <div id="line-direction" class="entur-top-padding">
-						<h3 class="text-left">{{itemValue('LineCode')}} - {{itemValue('Direction01_FrontDisplay')}}</h3>
-          </div>
+          <h2 class="text-left">{{stopPlaceName}}</h2>
+          <h3 class="text-left">{{lineCode}} - {{directionOne}}</h3>
         </div>
       </div>
 			<div class="row">
         <div class="col-md-1">
         </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure01_Time1') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure01_Time2') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure01_Time3') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure01_Time4') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure01_Time5') | date:'HH:mm'}}</h3>
-        </div>
-		</div>
-    <div id="direction02" class="entur-top-padding">
-			<div class="row">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-11">
-          <h3 class="text-left">{{itemValue('LineCode')}} - {{itemValue('Direction02_FrontDisplay')}}</h3>
-        </div>
+        <div class="col-md-2" data-ng-repeat="i in range">
+  				<h3 ng-style="{{directionOneDepartures[i].isLive}} ? {'color': config.realtime_color} : {'color': ''}">{{directionOneDepartures[i].time | date:'HH:mm:ss'}}</h3>
+      	</div>
       </div>
-			<div class="row">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure02_Time1') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure02_Time2') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure02_Time3') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure02_Time4') | date:'HH:mm'}}</h3>
-        </div>
-        <div class="col-md-2">
-          <h3 class="text-left">{{itemValue('RealTime_Departure02_Time5') | date:'HH:mm'}}</h3>
-        </div>
 		</div>
 	</div>
 </div>
